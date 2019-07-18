ed60daf4928d78686a0a4ce9bab93cfc2bcd7995
Jessica Minnier <jessica.minnier@gmail.com>
2019-07-18 10:44:45 -0700

    add m's examples, install file

---
+++ 00-install.R
@@ -0,0 +1,12 @@
+# install packages needed
+install.packages(c("tidyverse","knitr","rmarkdown",
+                   "devtools","kableExtra",
+                   "xaringan","here"))
+
+library(tidyverse)
+library(knitr)
+library(rmarkdown)
+library(devtools)
+library(kableExtra)
+library(here)
+
